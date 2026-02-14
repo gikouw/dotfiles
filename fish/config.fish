e32e1ae599bc3f5610b070cbf9c94cbd3a707353
gikouw <pengwinator0@gmail.com>
2026-02-14 11:53:05 +0530

    removed hardcoded path for local init script

--- fish/config.fish
+++ fish/config.fish
@@ -6,8 +6,10 @@ fish_vi_key_bindings
 alias e="x $EDITOR"
 alias c="f cd"
 
+set -l LOCAL_INIT run_local_startup_jobs
+
 if status is-login
-    if command -v run_local_jobs >/dev/null
-	run_local_jobs &
+    if command -v $LOCAL_INIT >/dev/null
+	$LOCAL_INIT &
     end
 end
